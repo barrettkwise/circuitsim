d220a1363aa6fd4d657ba407fc05a23a847e2f85
Barrett Wise <barrettkwise@gmail.com>
2023-01-08 17:27:51 -0500

    Update gateinterface.py

--- gateinterface.py
+++ gateinterface.py
@@ -27,25 +27,25 @@ class Gates():
         else:
             print("Gate not supported.")
 
-    def NOT(a) -> str:
+    def NOT(a: str) -> str:
         if a == "1":
             return "0"
         else:
             return "1"
 
-    def AND(a, b) -> str:
+    def AND(a: str , b: str) -> str:
         if a == "1" and b == "1":
             return "1"
         else:
             return "0"
 
-    def OR(a, b) -> str:
+    def OR(a: str , b: str) -> str:
         if a == "1" or b == "1":
             return "1"
         else:
             return "0"
 
-    def XOR(a, b) -> str:
+    def XOR(a: str , b: str) -> str:
         if a != b:
             return "1"
         else:
